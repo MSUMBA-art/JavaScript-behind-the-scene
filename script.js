feb216e0ac38196c896a6873769dd38959570c07
ALEXANDER <msumba83@gmail.com>
2023-08-30 06:05:43 +0200

    initial commit

--- script.js
+++ script.js
@@ -4,15 +4,26 @@ function calcAge(birthYear) {
   const age = 2023 - birthYear;
 
   function printAge() {
-    const output = `${firstName} ${sirName} is ${age} years old, born in ${birthYear} born in ${birthDistrict}, southern ${birthCountry}`;
+    let output = `${firstName} ${sirName} is ${age} years old, born in ${birthYear} born in ${birthDistrict}, southern ${birthCountry}`;
       console.log(output);
       
       if (birthYear >= 1981 && birthYear <= 1996) {
-          var millennial = true
+        var millennial = true
+        const firstName = "Lloyd"
+        console.log(add(10, 7));
           const str = `Ohh!, you are a millennial, ${firstName} ${sirName}`
-          console.log(str);
+        console.log(str);
+        
+        function add(a, b) {
+          return a + b
+        }
+      
       } 
-      console.log(millennial);
+    console.log(millennial);
+    console.log(output);
+    // console.log(add(10, 7));
+    
+   
   }
   printAge();
 
